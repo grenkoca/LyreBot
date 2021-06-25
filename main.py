1e708f28107c4365c412e095fdb691fd4b1f0639
grenkoca <grenkoca@gmail.com>
2021-06-24 23:41:24 -0400

    Created bot initialization, hooked into main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src import initialize_bot
 import yaml
+
+
 if __name__ == "__main__":
-    with open("config.yml", "ro") as f:
+    with open("config.yml", "r") as f:
         config_file = yaml.load(f)
-    initialize_bot(config=config_file)
+    initialize_bot()
